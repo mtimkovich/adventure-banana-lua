91c5b15ccb0d80e139160bef76184fd446546b5f
Max Timkovich <mtimkovich@gmail.com>
2012-08-21 16:39:55 -0500

    Added conf.lua file

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.title = "Adventure Banana"
+    t.screen.width = 800
+    t.screen.height = 600
+    t.screen.fullscreen = false
+    t.modules.joystick = false
+    t.modules.physics = false
+end
